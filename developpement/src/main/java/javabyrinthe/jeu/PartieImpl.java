75762041fdca466123ebae05a504ad2fa713fc62
Christophe Cluizel <christophe.cluizel@gmail.com>
2015-04-17 14:14:01 +0200

    style: clean the code and add indentations

--- developpement/src/main/java/javabyrinthe/jeu/PartieImpl.java
+++ developpement/src/main/java/javabyrinthe/jeu/PartieImpl.java
@@ -9,56 +9,56 @@ import javabyrinthe.labyrinthe.LabyrinthGenerator;
 import javabyrinthe.labyrinthe.Labyrinth;
 
 public class PartieImpl implements Partie, Remote {
-  public int nbJoueurMax = 2;
-  public int nbJoueur = 0;
-  public int nbTourMax = 3;
-  public int nbTour = 0;
+   int nbJoueurMax = 2;
+   int nbJoueur = 0;
+   int nbTourMax = 3;
+   int nbTour = 0;
   
-  Labyrinth labyrinth;
-  ArrayList<Joueur> joueurList;
+   Labyrinth labyrinth;
+   ArrayList<Joueur> joueurList;
 
-	public PartieImpl(int nbJoueurMax) throws RemoteException{
-		this.nbJoueurMax = nbJoueurMax;
-		this.joueurList = new ArrayList<Joueur>();
-		try {
-			this.labyrinth = new LabyrinthGenerator().loadLabyrinth("graph1.dot");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+   public PartieImpl(int nbJoueurMax) throws RemoteException{
+	   this.nbJoueurMax = nbJoueurMax;
+	   this.joueurList = new ArrayList<Joueur>();
+	   try {
+		   this.labyrinth = new LabyrinthGenerator().loadLabyrinth("graph1.dot");
+	   } catch (FileNotFoundException e) {
+		   e.printStackTrace();
+	   }
+   }
 
-	public Labyrinth getLabyrinthe() {
-		return labyrinth;
-	}
+   public Labyrinth getLabyrinthe() {
+	   return labyrinth;
+   }
 
-	public void tourSuivant() throws RemoteException {
-		nbTour++;
-		for(Joueur joueur : joueurList){
-			try{
-				String choix = joueur.jouer();
-				System.out.println("tour:"+nbTour+" jeu "+choix);
-			}catch(Exception e){
-				System.out.println("tour "+nbTour + " -> "+e);
-			}
-		}
-	}
+   public void tourSuivant() throws RemoteException {
+	   this.nbTour++;
+	   for(Joueur joueur : this.joueurList){
+		   try{
+			   String choixDeplacement = joueur.jouer();
+			   System.out.println("tour:" + this.nbTour + " jeu " + choixDeplacement);
+		   }catch(Exception e){
+			   System.out.println("tour " + this.nbTour + " -> " + e);
+		   }
+	   }
+   }
 
-  	public void addJoueur(Joueur joueur) throws RemoteException {
-  		joueurList.add(joueur);
-  		nbJoueur++;
-		System.out.println(nbJoueur +"/"+nbJoueurMax);
-  	}
+   public void addJoueur(Joueur joueur) throws RemoteException {
+	   this.joueurList.add(joueur);
+	   this.nbJoueur++;
+	   System.out.println(this.nbJoueur + "/" + this.nbJoueurMax);
+   }
 
-  	public int getnbJoueurMax(){
-  		return nbJoueurMax;
-  	}
-	public int getnbJoueur(){
-  		return nbJoueur;
-  	}
-	public int getnbTourMax(){
-  		return nbTourMax;
-  	}
-	public int getnbTour(){
-  		return nbTour;
-  	}
+   public int getnbJoueurMax(){
+	   return this.nbJoueurMax;
+   }
+   public int getnbJoueur(){
+	   return this.nbJoueur;
+   }
+   public int getnbTourMax(){
+	   return this.nbTourMax;
+   }
+   public int getnbTour(){
+	   return this.nbTour;
+   }
 }
