9b17856761cc8a6e5780e24cf9c8d54156123e65
Christophe Cluizel <christophe.cluizel@gmail.com>
2015-04-20 17:41:58 +0200

    feat(javabyrinthe): add an example of main to run a Slick2D window

--- developpement/src/main/java/javabyrinthe/MainClass.java
+++ developpement/src/main/java/javabyrinthe/MainClass.java
@@ -1,17 +1,49 @@
 package javabyrinthe;
-import java.io.FileNotFoundException;
 
-import javabyrinthe.labyrinthe.LabyrinthGenerator;
-import javabyrinthe.labyrinthe.Labyrinth;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class MainClass {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-	public static void main(String[] args) {
-		try {
-			Labyrinth laby = new LabyrinthGenerator().loadLabyrinth("src/main/resources/graph1.dot");
-			laby.getGraph().display();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+public class MainClass extends BasicGame
+{
+	public MainClass(String gamename)
+	{
+		super(gamename);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		g.drawString("Howdy!", 10, 10);
+	}
+
+	public static void main(String[] args)
+	{
+		System.setProperty("java.library.path", "lib");
+		System.setProperty("org.lwjgl.librarypath", new File("lib/native").getAbsolutePath());
+		
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new MainClass("Simple Slick Game"));
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 }
