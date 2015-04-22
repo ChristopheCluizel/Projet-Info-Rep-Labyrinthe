75bd1876d5a6951b1af6e1064f3110b7f7aeb60e
wallon <swallon@MacBook-Air-de-wallon.local>
2015-04-22 16:50:25 +0200

    feat(master) : ihm classes

---
+++ developpement/src/main/java/javabyrinthe/ihm/IHM.java
@@ -0,0 +1,37 @@
+package javabyrinthe.ihm;
+
+import java.io.File;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class IHM extends StateBasedGame{
+	private Menu jeu;
+    private AppGameContainer container;
+
+	public IHM() {
+        super("This is a test");
+    }
+
+    @Override
+    public void initStatesList(GameContainer container) throws SlickException{
+        addState(new Menu());
+    }
+
+    public static void main(String[] args) throws SlickException{
+    	System.setProperty("java.library.path","lib");
+    	System.setProperty("org.lwjgl.librarypath",new File("lib/nativeOSX").getAbsolutePath());
+        try{
+            AppGameContainer container = new AppGameContainer(new IHM());
+            container.setDisplayMode(600,600,false);
+            container.setTargetFrameRate(60);
+            container.start();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
