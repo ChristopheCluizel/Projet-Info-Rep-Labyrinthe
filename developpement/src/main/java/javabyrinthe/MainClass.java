43c374989074af5684f83ab0d7dceb98f0a66084
Christophe Cluizel <christophe.cluizel@gmail.com>
2015-05-05 22:21:03 +0200

    feat(javabyrinthe): clean MainClass

--- developpement/src/main/java/javabyrinthe/MainClass.java
+++ developpement/src/main/java/javabyrinthe/MainClass.java
@@ -1,21 +1,8 @@
 package javabyrinthe;
 
-import java.rmi.RemoteException;
-
-import javabyrinthe.jeu.CompilationFailedException;
-import javabyrinthe.jeu.JoueurIA;
-
 public class MainClass {
 
 	public static void main(String[] args) {
-		String code = "";
-		try {
-			JoueurIA ia = new JoueurIA("christophe", code);
-			ia.init();
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		} catch (CompilationFailedException e) {
-			e.printStackTrace();
-		}
+		
 	}
 }
