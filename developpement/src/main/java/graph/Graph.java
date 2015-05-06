d312cedae5327179325b4996be2b9275c494b5e1
Christophe Cluizel <christophe.cluizel@gmail.com>
2015-05-06 16:02:14 +0200

    feat(graph): add toString and toStringForUser methods, add nodes and nbEdges attributes

--- developpement/src/main/java/graph/Graph.java
+++ developpement/src/main/java/graph/Graph.java
@@ -6,7 +6,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 /**
- * Graph is the class to represent a graph. It uses a list of adjacence nodes to be lighter in memory
+ * Graph is the class to represent a graph. It uses a list of adjacence nodes to
+ * be lighter in memory
  * 
  * @author christophe
  */
@@ -15,48 +16,58 @@ public class Graph implements Serializable {
 	String name;
 	Integer nbEdge;
 	HashMap<Integer, ArrayList<Integer>> adjacence;
-	Integer size;
-	
+	HashMap<Integer, Integer> nodes;
+
 	/**
 	 * Construct an empty Graph
 	 * 
 	 * @param name The name of the graph
 	 * @param nbEdge The number of edges of the graph
 	 */
-	public Graph(String name, Integer nbEdge) {
+	public Graph(String name) {
 		this.name = name;
-		this.nbEdge = nbEdge;
+		this.nbEdge = 0;
 		this.adjacence = new HashMap<Integer, ArrayList<Integer>>();
+		this.nodes = new HashMap<Integer, Integer>();
 	}
-	
+
 	/**
 	 * Return all the nodes of the graph
 	 * 
 	 * @return a map of all the nodes of the graph
 	 */
-	public HashMap<Integer, ArrayList<Integer>> getAdjacence() {
-		return this.adjacence;
+	public HashMap<Integer, Integer> getNodes() {
+		return this.nodes;
 	}
-	
+
 	/**
-	 * 	Return the number of nodes of the graph
+	 * Return the number of nodes in the graph
 	 * 
-	 * @return the number of nodes of the graph
+	 * @return the number of nodes in the graph
 	 */
-	public Integer getSize() {
-		return this.size;
+	public Integer numberOfNodes() {
+		return this.nodes.size();
 	}
-	
+
+	/**
+	 * Return the number of edges in the graph
+	 * 
+	 * @return the number of edges in the graph
+	 */
+	public Integer getNumberOfEdges() {
+		return this.nbEdge;
+	}
+
 	/**
 	 * Add a node in the graph
 	 * 
 	 * @param key the key of the node to add
 	 */
 	public void addNode(Integer key) {
+		this.nodes.put(key, key);
 		this.adjacence.put(key, new ArrayList<Integer>());
-		this.setSize();
 	}
-	
+
 	/**
 	 * Add an edge in the graph
 	 * 
@@ -66,8 +77,9 @@ public class Graph implements Serializable {
 	 */
 	public void addEdge(Integer key1, Integer key2, Integer value) {
 		this.adjacence.get(key1).add(key2);
+		this.nbEdge += 1;
 	}
-	
+
 	/**
 	 * Get the successors of a node
 	 * 
@@ -77,7 +89,7 @@ public class Graph implements Serializable {
 	public ArrayList<Integer> getSuccessors(Integer key) {
 		return this.adjacence.get(key);
 	}
-	
+
 	/**
 	 * Test if a node is present or not in the graph
 	 * 
@@ -87,22 +99,45 @@ public class Graph implements Serializable {
 	public Boolean nodePresent(Integer key) {
 		return adjacence.containsKey(key);
 	}
-	
+
 	/**
 	 * Display all the nodes of the graph with its successors
 	 */
 	public void display() {
 		Iterator<Integer> i = this.adjacence.keySet().iterator();
-		Integer clef = null;
-		while (i.hasNext())
-		{
-		    clef = (Integer)i.next();
-		    System.out.println("key : " + clef + ", Successors : " +
-		    		getSuccessors(clef).toString());
+		Integer key = null;
+		while (i.hasNext()) {
+			key = (Integer) i.next();
+			System.out.println("key : " + key + ", Successors : " + getSuccessors(key).toString());
 		}
 	}
-	
-	private void setSize() {
-		this.size = this.adjacence.size();
+
+	/**
+	 * Redefine the toString method to describe a graph.
+	 */
+	@Override
+	public String toString() {
+		String string = getNumberOfEdges().toString() + "\n" + "graph " + name + " {\n";
+		Iterator<Integer> i = this.adjacence.keySet().iterator();
+		while (i.hasNext()) {
+			Integer key = (Integer) i.next();
+			for (int j = 0; j < this.adjacence.get(key).size(); j++) {
+				string += key.toString() + " -> " + this.adjacence.get(key).get(j).toString() + "\n";
+			}
+		}
+		string += "}";
+		return string;
+	}
+
+	public String toStringForUser() {
+		String string = getNumberOfEdges().toString() + "\n";
+		Iterator<Integer> i = this.adjacence.keySet().iterator();
+		while (i.hasNext()) {
+			Integer key = (Integer) i.next();
+			for (int j = 0; j < this.adjacence.get(key).size(); j++) {
+				string += key.toString() + " " + this.adjacence.get(key).get(j).toString() + "\n";
+			}
+		}
+		return string.substring(0, string.length() - 1);
 	}
 }
