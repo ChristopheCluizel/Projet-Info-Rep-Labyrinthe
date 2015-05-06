50df2124313c93955bbe007ac2c669cf0a721e08
Christophe Cluizel <christophe.cluizel@gmail.com>
2015-05-06 16:04:08 +0200

    feat(labyrinthe): update classes with toString and toStringForUser methods, add width and height attributes

--- developpement/src/main/java/javabyrinthe/labyrinthe/Labyrinth.java
+++ developpement/src/main/java/javabyrinthe/labyrinthe/Labyrinth.java
@@ -13,12 +13,15 @@ import graph.Graph;
  */
 public class Labyrinth implements Serializable {
 	Graph graph;
-	Integer size;
+	String name;
+	int width;
+	int height;
 	Coordinate departure;
 	Coordinate arrival;
-	
+
 	/**
-	 * Create a labyrinth with a graph conception, a departure square and an arrival square
+	 * Create a labyrinth with a graph conception, a departure square and an
+	 * arrival square
 	 * 
 	 * @param graph the graph of the labyrinth
 	 * @param departure the coordinate of the departure square
@@ -26,15 +29,51 @@ public class Labyrinth implements Serializable {
 	 * @see Graph
 	 * @see Coordinate
 	 */
-	public Labyrinth(Graph graph, Coordinate departure, Coordinate arrival) {
+	public Labyrinth(Graph graph, Coordinate departure, Coordinate arrival, int width, int height, String name) {
 		this.graph = graph;
+		this.name = name;
 		this.departure = departure;
 		this.arrival = arrival;
-		this.size = (int)Math.sqrt(graph.getSize());
+		this.width = width;
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		return this.name + " " + this.width + " " + this.height + "\n" + this.departure.getX() + " "
+				+ this.departure.getY() + "\n" + this.arrival.getX() + " " + this.arrival.getY() + "\n"
+				+ graph.toString();
 	}
 	
+	public String toStringForUser() {
+		return this.width + " " + this.height + "\n" + this.departure.getX() + " "
+				+ this.departure.getY() + "\n" + this.arrival.getX() + " " + this.arrival.getY() + "\n"
+				+ graph.toStringForUser();
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 	/**
-	 * Test if 2 squares are adjacent without a wall between them, so if it's an authorized move
+	 * Test if 2 squares are adjacent without a wall between them, so if it's an
+	 * authorized move
 	 * 
 	 * @param departure the coordinate of the departure square
 	 * @param arrival the coordinate of the arrival square
@@ -44,13 +83,13 @@ public class Labyrinth implements Serializable {
 	 */
 	public Boolean isAuthorizedMove(Coordinate departure, Coordinate arrival) {
 		LabyrinthGenerator labGenerator = new LabyrinthGenerator();
-		Integer keyDeparture = labGenerator.coordinatesToKey(departure, this.size);
-		Integer keyArrival = labGenerator.coordinatesToKey(arrival, this.size);
+		Integer keyDeparture = labGenerator.coordinatesToKey(departure, this.width);
+		Integer keyArrival = labGenerator.coordinatesToKey(arrival, this.width);
 		System.out.println("Déplacement: " + keyDeparture + " -> " + keyArrival);
-		
+
 		return this.graph.getSuccessors(keyDeparture).contains(keyArrival);
 	}
-	
+
 	/**
 	 * Get the graph of the labyrinth
 	 * 
@@ -62,12 +101,12 @@ public class Labyrinth implements Serializable {
 	}
 
 	/**
-	 * Get the size of the labyrinth
+	 * Get the number of squares of the labyrinth
 	 * 
-	 * @return the size of the labyrinth
+	 * @return the number of squares of the labyrinth
 	 */
 	public Integer getSize() {
-		return this.size;
+		return this.graph.getNodes().size();
 	}
 
 	/**
@@ -89,7 +128,7 @@ public class Labyrinth implements Serializable {
 	public Coordinate getArrival() {
 		return this.arrival;
 	}
-	
+
 	/**
 	 * Set the arrival square coordinate of the labyrinth
 	 * 
@@ -99,9 +138,9 @@ public class Labyrinth implements Serializable {
 	public void setArrival(Coordinate arrival) {
 		this.arrival = arrival;
 	}
-	
+
 	/**
-	 * Return the coordinate of the next square according to a move order 
+	 * Return the coordinate of the next square according to a move order
 	 * 
 	 * @param order the order of move (HAUT, BAS, GAUCHE, DROITE)
 	 * @param actualPosition the coordinate of the actual position
@@ -109,18 +148,18 @@ public class Labyrinth implements Serializable {
 	 * @see Coordinate
 	 */
 	public Coordinate getNextSquareFromOrder(String order, Coordinate actualPosition) {
-		
-		switch(order) {
-			case "HAUT":
-				return new Coordinate(actualPosition.getX(), Math.max(actualPosition.getY() - 1, 0));
 
-			case "BAS":
-				return new Coordinate(actualPosition.getX(), Math.min(actualPosition.getY() + 1, this.getSize()-1));
-			case "GAUCHE":
-				return new Coordinate(Math.max(0, actualPosition.getX() - 1), actualPosition.getY());
-				
-			case "DROITE":
-				return new Coordinate(Math.min(this.getSize()-1, actualPosition.getX() + 1), actualPosition.getY());
+		switch (order) {
+		case "HAUT":
+			return new Coordinate(actualPosition.getX(), Math.max(actualPosition.getY() - 1, 0));
+
+		case "BAS":
+			return new Coordinate(actualPosition.getX(), Math.min(actualPosition.getY() + 1, this.getSize() - 1));
+		case "GAUCHE":
+			return new Coordinate(Math.max(0, actualPosition.getX() - 1), actualPosition.getY());
+
+		case "DROITE":
+			return new Coordinate(Math.min(this.getSize() - 1, actualPosition.getX() + 1), actualPosition.getY());
 		}
 		return null;
 	}
--- developpement/src/main/java/javabyrinthe/labyrinthe/LabyrinthGenerator.java
+++ developpement/src/main/java/javabyrinthe/labyrinthe/LabyrinthGenerator.java
@@ -11,12 +11,13 @@ import java.io.Serializable;
 import javabyrinthe.tools.Coordinate;
 
 /**
- * LabyrinthGenerator allows to load a labyrinth from a file, to permute between key node of the graph and coordinate of the labyrinth and vice versa
+ * LabyrinthGenerator allows to load a labyrinth from a file, to permute between
+ * key node of the graph and coordinate of the labyrinth and vice versa
  * 
  * @author christophe
  */
-public class LabyrinthGenerator implements Serializable{
-	
+public class LabyrinthGenerator implements Serializable {
+
 	/**
 	 * Load a labyrinth from a text file
 	 * 
@@ -28,40 +29,51 @@ public class LabyrinthGenerator implements Serializable{
 	public Labyrinth loadLabyrinth(String filePath) throws FileNotFoundException {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(filePath));
+			String[] nameSizeArray = reader.readLine().split(" ");
+			String name = nameSizeArray[0];
+			int width = Integer.parseInt(nameSizeArray[1]);
+			int height = Integer.parseInt(nameSizeArray[2]);
+			String[] departureArray = reader.readLine().split(" ");
+			Coordinate departure = new Coordinate(Integer.parseInt(departureArray[0]), Integer.parseInt(departureArray[1]));
+			String[] arrivalArray = reader.readLine().split(" ");
+			Coordinate arrival = new Coordinate(Integer.parseInt(arrivalArray[0]), Integer.parseInt(arrivalArray[1]));
+
 			Integer nbEdges = Integer.parseInt(reader.readLine());
-//			System.out.println("nb edges : " + nbEdges);
-			String firstLine = reader.readLine().toString();
-			String[] result = firstLine.split(" ");
-			String graphName = result[1];
-			
-			Graph graph = new Graph(graphName, nbEdges);
-			for(int i = 0; i < nbEdges; i++) {
+			String graphName = reader.readLine().split(" ")[1];
+
+			Graph graph = new Graph(graphName);
+			for (int i = 0; i < nbEdges; i++) {
 				String[] keys = reader.readLine().split(" -> ");
-				if(!graph.nodePresent(Integer.parseInt(keys[0])))
-					graph.addNode(Integer.parseInt((keys[0])));
-				graph.addEdge(Integer.parseInt(keys[0]), Integer.parseInt(keys[1]), 1);
+				int key1 = Integer.parseInt(keys[0]);
+				int key2 = Integer.parseInt(keys[1]);
+				if (!graph.nodePresent(key1))
+					graph.addNode(key1);
+				if (!graph.nodePresent(key2))
+					graph.addNode(key2);
+				graph.addEdge(key1, key2, 1);
 			}
 			reader.close();
-//			System.out.println("size laby: " + graph.getSize());
-			return new Labyrinth(graph, new Coordinate((int)Math.sqrt(graph.getSize()) / 2, (int)Math.sqrt(graph.getSize()) / 2), new Coordinate(0, 0)); // the departure is the center of the labyrinth
+			// System.out.println("size laby: " + graph.getSize());
+			return new Labyrinth(graph, departure, arrival, width, height, name);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Turn a key node of the graph into a coordinate of a square of the labyrinth
 	 * 
 	 * @param key the key of a node of the graph to convert in coordinates
 	 * @param graphWidth the size of the graph (height and width are equal)
-	 * @return a couple (x,y) which are the coordinates of a square of the labyrinth
+	 * @return a couple (x,y) which are the coordinates of a square of the
+	 *         labyrinth
 	 * @see Coordinate
 	 */
 	public Coordinate keyToCoordinates(Integer key, Integer graphWidth) {
 		return new Coordinate(key % graphWidth, key / graphWidth);
 	}
-	
+
 	/**
 	 * Turn a coordinate of a squate of the labyrinth into a key node of the graph
 	 * 
